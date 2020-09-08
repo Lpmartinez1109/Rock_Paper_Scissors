8da6169510e8918809d60ef6f8ac29867b776c6f
LPMartinez <lpmartinez1109@gmail.com>
2020-09-08 09:59:53 -0500

    random asspects added to file. computer and user variable added to repo,

---
+++ rock_paper_scissors.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdlib.h>
+
+int main(){
+    srand (time(NULL));
+    int computer = rand() % 3 + 1;
+    int user = 0;
+
+    std::cout << "====================\n";
+    std::cout << "rock paper scissors!\n";
+    std::cout << "====================\n";
+
+
+};
