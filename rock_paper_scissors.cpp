a12adede41e18035f12e3bd4464feb4c7b914b8e
LPMartinez <lpmartinez1109@gmail.com>
2020-09-08 10:12:37 -0500

    Added if else conditionals to repo file

--- rock_paper_scissors.cpp
+++ rock_paper_scissors.cpp
@@ -10,5 +10,34 @@ int main(){
     std::cout << "rock paper scissors!\n";
     std::cout << "====================\n";
 
+std::cout << "1) ✊\n";
+  std::cout << "2) ✋\n";
+  std::cout << "3) ✌️\n";
+
+  std::cout << "shoot! ";
+  std::cin >> user;
+
+  if (user == 1 && computer == 1){
+    std::cout << "You chose Rock! Your Opponent Chose Rock! You have tied! \n";
+  } else if (user == 1 && computer == 2){
+    std::cout << "You Chose Rock! Your Opponent Chose Paper! You Have Lost! \n";
+    }else if(user == 1 && computer == 3){
+      std::cout << "You Chose Rock! Your Opponent Choose Scissors! You Have Won! \n";
+    }
+    else if (user == 2 && computer == 1){
+      std::cout << "You Chose Paper! Your Opponent chose Rock! You Have Won! \n";
+    }else if (user == 2 && computer == 2){
+      std::cout << "You Chose Paper! Your Opponent Chose Paper! You have Tied! \n";
+    }else if (user == 2 && computer == 3){
+      std::cout << "You Chose Paper! Your Opponent Chose Scissor! You Have Lost! \n";
+    }else if (user == 3 && computer == 1) {
+      std::cout << "You Chose Scissors! Your Opponent Chose Rock! You Have Lost! \n";
+    }else if (user == 3 && computer == 2) {
+      std::cout << "You Chose Scissors! Your Opponent Chose Paper! You Have Won! \n";
+    }else if (user == 3 && computer == 3){
+      std::cout << "You Chose Sciossors! Your Opponent Chose Scissors! You Have Tied! \n";
+    }
+
+
 
 };
